e13d17c39b8ed0b632fb24f389d0138c51c0b833
Mohammed Al-Tameemi <m.r.altameemi@gmail.com>
2022-01-15 16:30:21 -0500

    added shading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod primitives;
 mod objects;
 
+use num::integer::sqrt;
 use primitives::{color::Color, vec::{Vec, Vec3}, ray::Ray};
 use objects::{image::Image, camera::Camera};
 use image::{RgbImage, Rgb};
@@ -26,20 +27,26 @@ fn main() {
 }
 
 fn ray_color(ray: Ray) -> Color {
-    if hits_sphere(ray, Vec::new(0.0, 0.0, -1.0), 0.5) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hits_sphere(ray, Vec::new(0.0, 0.0, -1.0), 0.5);
+    if t > 0.0 {
+        let n = (ray.at(t) - Vec::new(0.0, 0.0, -1.0)).unit_vector();
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
 
     let unit_direction = ray.direction.unit_vector();
-    let t = 0.5 * (unit_direction.y() + 1.0) + 0.5 * (unit_direction.x() + 1.0);
-    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0);
 }
 
-fn hits_sphere(ray: Ray, center: Vec, radius: f64) -> bool {
+fn hits_sphere(ray: Ray, center: Vec, radius: f64) -> f64 {
     let oc = ray.origin - center;
     let a = ray.direction.dot(ray.direction);
     let b = 2.0 * oc.dot(ray.direction);
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
