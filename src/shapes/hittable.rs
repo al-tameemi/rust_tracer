31f741cc4b038e035c504fd7b16d2ec963895924
Mohammed Al-Tameemi <m.r.altameemi@gmail.com>
2022-01-16 13:00:55 -0500

    added a sphere struct and a hittable trait

---
+++ src/shapes/hittable.rs
@@ -0,0 +1,37 @@
+use crate::primitives::{ray::Ray, vector::Vector};
+
+pub trait Hittable {
+    fn hit(&self, ray: Ray, t_min: f64, t_max: f64, hit_record: &mut HitRecord) -> bool;
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct HitRecord {
+    pub point: Option<Vector>,
+    pub normal: Option<Vector>,
+    pub t: Option<f64>,
+    pub front_face: Option<bool>,
+}
+
+impl HitRecord {
+    pub fn new() -> HitRecord {
+        HitRecord {
+            point: None,
+            normal: None,
+            t: None,
+            front_face: None,
+        }
+    }
+
+    pub fn set_face_normal(&mut self, ray: &Ray, outward_normal: Vector) {
+        self.front_face = Some(ray.direction.dot(outward_normal) < 0.0);
+        self.normal = match self.front_face {
+            Some(boolean) => {
+                match boolean {
+                    true => {Some(outward_normal)},
+                    false => {Some(-outward_normal)}
+                }
+            },
+            _ => {None}
+        };
+    }
+}
---
+++ src/shapes/sphere.rs
@@ -0,0 +1,48 @@
+use super::hittable::{Hittable, HitRecord};
+use crate::primitives::{vector::Vector, ray::Ray};
+pub struct Sphere {
+    radius: f64,
+    center: Vector,
+}
+
+impl Sphere {
+    pub fn new(center: Vector, radius: f64) -> Sphere {
+        Sphere { 
+            radius, 
+            center 
+        }
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, ray: Ray, t_min: f64, t_max: f64, mut hit_record: &mut HitRecord) -> bool {
+        let oc = ray.origin - self.center;
+        let a = ray.direction.length_squared();
+        let half_b = oc.dot(ray.direction);
+        let c = oc.length_squared() - self.radius * self.radius;
+
+        let discriminant = half_b * half_b - a * c;
+        
+        if discriminant < 0.0 {
+            return false;
+        }
+
+        let sqrt_discriminant = discriminant.sqrt();
+
+
+        // Find the nearest root that lies in the acceptable range.
+        let mut root = (-half_b - sqrt_discriminant) / a;
+        if root < t_min || t_max < root {
+            root = (-half_b + sqrt_discriminant) / a;
+            if root < t_min || t_max < root {
+                return false;
+            }
+        }
+
+        hit_record.t = Some(root);
+        hit_record.point = Some(ray.at(root));
+        let outward_normal = (hit_record.point.unwrap() - self.center) / self.radius;
+        hit_record.set_face_normal(&ray, outward_normal);
+        return true;
+    }
+}
