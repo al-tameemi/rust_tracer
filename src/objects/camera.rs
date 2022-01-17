b079edba90120a5954e7b786b76fcf5adabe5f52
Mohammed Al-Tameemi <m.r.altameemi@gmail.com>
2022-01-17 03:45:40 -0500

    removed uneeded member variables

--- src/objects/camera.rs
+++ src/objects/camera.rs
@@ -1,10 +1,7 @@
 use super::image::Image;
-use crate::primitives::vector::{Vector, Vec3};
+use crate::primitives::{vector::{Vector, Vec3}, ray::Ray};
 
 pub struct Camera {
-    pub viewport_height: f64,
-    pub viewport_width: f64,
-    pub focal_length: f64,
     pub origin: Vector,
     pub horizontal: Vector,
     pub vertical: Vector,
@@ -23,13 +20,17 @@ impl Camera {
         let lower_left_corner = origin - horizontal / 2.0 as f64 - vertical / 2.0 as f64 - Vector::new(0.0, 0.0, focal_length);
         
         Camera { 
-            viewport_height, 
-            viewport_width, 
-            focal_length, 
             origin, 
             horizontal, 
             vertical, 
             lower_left_corner 
+        }
+    }
+
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        Ray { 
+            origin: self.origin, 
+            direction: self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin 
         }
     }
 }
